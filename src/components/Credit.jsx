9a8eb2c49bee15cd9555c0ffcd9167db44b3fe45
unknown <sizan14789@gmail.com>
2024-12-03 08:58:00 +0600

    Final commit

--- src/components/Credit.jsx
+++ src/components/Credit.jsx
@@ -2,7 +2,7 @@ export const Credit = ()=>{
   return(
     <div className="credit" >
       <p>Made by <a href="https://www.facebook.com/sizan14789/">Sizan Molla</a></p>
-      <button>Source Code</button>
+      <button><a href="https://github.com/Sizan147/The-Dice">Source Code</a></button>
     </div>
   )
 }
